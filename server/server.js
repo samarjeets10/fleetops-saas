8f89352b0e73882e01dbda9f70c44af93b51cb4a
Samarjeet <samarsabale1021@gmail.com>
2026-09-01 01:49:33 +0530

    refactor: improve formatting and structure of server start and shutdown functions

--- server/server.js
+++ server/server.js
@@ -2,24 +2,37 @@ const env = require("./src/config/env");
 const logger = require("./src/config/logger");
 const app = require("./src/app");
 const { connectDB, disconnectDB } = require("./src/config/database");
-const { preprocess } = require("zod");
 
 let server;
 
 async function start() {
-    try {
-        await connectDB();
+  try {
+    await connectDB();
 
-        server = app.listen(env.PORT, () => {
-            logger.info(`Server listening on port ${env.PORT}`), {
-                env: env.NODE_ENV,
-            };
-        });
-    } catch (error) {
-        logger.error("Failed to start server", { error: error.message });
-        process.exit(1);
-    }
+    server = app.listen(env.PORT, () => {
+      logger.info(`Server listening on port ${env.PORT}`, {
+        env: env.NODE_ENV,
+      });
+    });
+  } catch (error) {
+    logger.error("Failed to start server", { error: error.message });
+    process.exit(1);
+  }
+}
 
+async function shutdown(signal) {
+  logger.info(`Received ${signal}, shutting down gracefully`);
+
+  if (!server) {
+    process.exit(0);
+    return;
+  }
+
+  server.close(async () => {
+    await disconnectDB();
+    logger.info("Shutdown complete");
+    process.exit(0);
+  });
 }
 
 process.on("SIGTERM", () => shutdown("SIGTERM"));
